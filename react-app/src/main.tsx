e9eb11873de76452b20e8a17be9744aaad9baac8
Bou Zeid Elie <eliebouzeidd@gmail.com>
2025-06-22 16:14:29 +0200

    latest update

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import "./styles/custom.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./auth-context.tsx";
 import { SidebarProvider } from "./components/ui/sidebar.tsx";
 import { MySidebar } from "./mysidebar.tsx";
-import { Toaster } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
